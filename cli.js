4a839b5c13ee21ae786f9af6c2706175a74b8535
Daniil Tsivinsky <48863576+tsivinsky@users.noreply.github.com>
2020-02-10 16:55:55 +0300

    Move function createWebpackReact to separate file

--- cli.js
+++ cli.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 
-// Require fs module
-const fs = require("fs");
-const shell = require("shelljs");
+// Require functions for create projects
+const createWebpackReact = require("./modules/react-webpack");
 
-//! Arguments
-// 1. Project Type
-// 2. Name of new project
-// 3. Additional options
+/* Arguments
+  1. Project Type
+  2. Name of new project
+  3. Additional options
+*/
 
 // Get arguments from user
 const [, , ...args] = process.argv;
@@ -15,121 +15,19 @@ const [, , ...args] = process.argv;
 const projectType = args[0];
 const projectName = args[1];
 
-const htmlTemplate = fs.readFileSync("files/index.html");
-const indexJSTemplate = fs.readFileSync("files/index.js");
-const AppJSXTemplate = fs.readFileSync("files/App.jsx");
+// Get current user directory
+const userDir = process.cwd();
 
 switch (projectType) {
   case "react-webpack":
     if (args[1]) {
-      createWebpackReact();
+      createWebpackReact(userDir, projectName);
     } else {
-      console.log("Type a name of project");
+      console.log("Type a valid name of project");
     }
     break;
 
   default:
-    console.log("Type a type of project after create-project");
+    console.log("Type a type of project after new-project command");
     break;
-}
-
-function createWebpackReact() {
-  // Array of packages for new project
-  const packages = [
-    "webpack",
-    "webpack-cli",
-    "webpack-dev-server",
-    "@babel/core",
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "babel-loader",
-    "css-loader",
-    "html-webpack-plugin",
-    "jsx-loader",
-    "node-sass",
-    "sass-loader",
-    "style-loader",
-    "url-loader"
-  ];
-
-  // Object of name and value for new project
-  const scripts = {
-    start: "webpack-dev-server --mode development --open --hot",
-    build: "webpack --mode production"
-  };
-
-  // Check folder of project name for exists
-  if (fs.existsSync(`${__dirname}/${projectName}`)) {
-    return console.log(`Folder with name \"${projectName}\" already exists.`);
-  }
-
-  // Create a project folder
-  fs.mkdirSync(`${__dirname}/${projectName}`);
-
-  // Cd to a new project folder and initialize it
-  shell.exec(`cd ${projectName} && npm init -y`);
-
-  console.log("Now, script is installing dependencies to your new project");
-
-  // Install all dependencies
-  packages.forEach(package =>
-    shell.exec(`cd ${projectName} && npm install ${package}`)
-  );
-
-  console.log(
-    "All dependencies was installed and now script is setting up your new project"
-  );
-
-  // Read packaje.json file in new directory
-  const packageJSON = JSON.parse(
-    fs.readFileSync(`${__dirname}/${projectName}/package.json`),
-    "utf-8"
-  );
-
-  // Add scripts to package.json file in new directory
-  packageJSON.scripts.start = scripts.start;
-  packageJSON.scripts.build = scripts.build;
-  fs.writeFileSync(
-    `${__dirname}/${projectName}/package.json`,
-    JSON.stringify(packageJSON, null, 2),
-    "utf-8"
-  );
-
-  // Read boilerplate files
-  // Read webpack.config.js file
-  const webpackConfig = fs.readFileSync("files/webpack.config.js", "utf-8");
-
-  // Read .babelrc file
-  const babelConfig = fs.readFileSync("files/.babelrc", "utf-8");
-
-  // Create webpack and babel files in new project folder
-  fs.appendFileSync(
-    `${__dirname}/${projectName}/webpack.config.js`,
-    webpackConfig
-  );
-  fs.appendFileSync(`${__dirname}/${projectName}/.babelrc`, babelConfig);
-
-  // Create a src folder
-  fs.mkdirSync(`${__dirname}/${projectName}/src`);
-
-  // Create index.html file
-  fs.appendFileSync(
-    `${__dirname}/${projectName}/src/index.html`,
-    htmlTemplate,
-    "utf-8"
-  );
-
-  // Create index.js file
-  fs.appendFileSync(
-    `${__dirname}/${projectName}/src/index.js`,
-    indexJSTemplate,
-    "utf-8"
-  );
-
-  // Create App.jsx file
-  fs.appendFileSync(
-    `${__dirname}/${projectName}/src/App.jsx`,
-    AppJSXTemplate,
-    "utf-8"
-  );
 }
---
+++ modules/react-webpack.js
@@ -0,0 +1,130 @@
+// Require modules
+const fs = require("fs");
+const shell = require("shelljs");
+
+module.exports = function createWebpackReact(userDir, projectName) {
+  // Templates
+  const htmlTemplate = fs.readFileSync(
+    __dirname + "/../templates/react-webpack/index.html"
+  );
+  const indexJSTemplate = fs.readFileSync(
+    __dirname + "/../templates/react-webpack/index.js"
+  );
+  const AppJSXTemplate = fs.readFileSync(
+    __dirname + "/../templates/react-webpack/App.jsx"
+  );
+  const webpackConfig = fs.readFileSync(
+    `${__dirname}/../templates/react-webpack/webpack.config.js`,
+    "utf-8"
+  );
+  const babelConfig = fs.readFileSync(
+    `${__dirname}/../templates/react-webpack/.babelrc`,
+    "utf-8"
+  );
+
+  // Array of packages for new project
+  const packages = ["react", "react-dom"];
+  const devPackages = [
+    "webpack",
+    "webpack-cli",
+    "webpack-dev-server",
+    "@babel/core",
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "babel-loader",
+    "css-loader",
+    "html-webpack-plugin",
+    "jsx-loader",
+    "node-sass",
+    "sass-loader",
+    "style-loader",
+    "url-loader"
+  ];
+
+  // Object of name and value for new project
+  const scripts = {
+    start: "webpack-dev-server --mode development --open --hot",
+    build: "webpack --mode production"
+  };
+
+  // Check folder of project name for existing
+  if (fs.existsSync(`${userDir}/${projectName}`)) {
+    return console.log(`Folder with name \"${projectName}\" already exists.`);
+  }
+
+  // Create a project folder
+  fs.mkdirSync(`${userDir}/${projectName}`);
+
+  // Cd to a new project folder and initialize it
+  shell.exec(`cd ${projectName} && npm init -y`);
+
+  console.log("Now, script is installing dependencies to your new project");
+
+  // Install usual dependencies
+  let packagesString = "";
+  packages.forEach(package => {
+    packagesString += `${package} `;
+  });
+  shell.exec(`cd ${projectName} && npm install ${packagesString}`);
+
+  console.log(
+    "Usual dependencies was installed and dev dependencies will install now."
+  );
+
+  // Install dev dependencies
+  let devPackagesString = "";
+  devPackages.forEach(package => {
+    devPackagesString += `${package} `;
+  });
+  shell.exec(`cd ${projectName} && npm install ${devPackagesString}`);
+
+  console.log(
+    "All dependencies was installed and now script is setting up your new project"
+  );
+
+  // Read package.json file in new directory
+  const packageJSON = JSON.parse(
+    fs.readFileSync(`${userDir}/${projectName}/package.json`),
+    "utf-8"
+  );
+
+  // Add scripts to package.json file in new directory
+  packageJSON.scripts.start = scripts.start;
+  packageJSON.scripts.build = scripts.build;
+  fs.writeFileSync(
+    `${userDir}/${projectName}/package.json`,
+    JSON.stringify(packageJSON, null, 2),
+    "utf-8"
+  );
+
+  // Create webpack and babel files in new project folder
+  fs.appendFileSync(
+    `${userDir}/${projectName}/webpack.config.js`,
+    webpackConfig
+  );
+  fs.appendFileSync(`${userDir}/${projectName}/.babelrc`, babelConfig);
+
+  // Create a src folder
+  fs.mkdirSync(`${userDir}/${projectName}/src`);
+
+  // Create index.html file
+  fs.appendFileSync(
+    `${userDir}/${projectName}/src/index.html`,
+    htmlTemplate,
+    "utf-8"
+  );
+
+  // Create index.js file
+  fs.appendFileSync(
+    `${userDir}/${projectName}/src/index.js`,
+    indexJSTemplate,
+    "utf-8"
+  );
+
+  // Create App.jsx file
+  fs.appendFileSync(
+    `${userDir}/${projectName}/src/App.jsx`,
+    AppJSXTemplate,
+    "utf-8"
+  );
+};
