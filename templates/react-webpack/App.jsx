226d6d7a4d525aa586c691b320381b653719e007
Daniil Tsivinsky <48863576+tsivinsky@users.noreply.github.com>
2020-02-10 16:56:22 +0300

    Change structure of templates folder

---
+++ templates/react-webpack/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>
+        Edit <code>App.jsx</code> file to start!
+      </h1>
+    </div>
+  );
+}
