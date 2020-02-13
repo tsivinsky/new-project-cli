745c7ffcc2edac85a626d2a032c9ef9b0fd7d016
Daniil Tsivinsky <48863576+tsivinsky@users.noreply.github.com>
2020-02-13 14:17:05 +0300

    Little refactoring

--- functions.js
+++ functions.js
@@ -1,32 +1,24 @@
 const fs = require("fs");
 
-function splitPackages(packages, projectName, dev) {
-  let packagesString = "";
-  packages.forEach(package => {
-    packagesString += `${package} `;
-  });
-  if (dev) {
-    return `cd ${projectName} && npm install ${packagesString} --save-dev`;
-  } else {
-    return `cd ${projectName} && npm install ${packagesString}`;
-  }
-}
-
-function addFile(path, file) {
-  fs.appendFileSync(path, file, "utf-8");
-}
-
-function readFile(path) {
-  return fs.readFileSync(path, "utf-8");
-}
-
-function writeJson(path, file) {
-  fs.writeFileSync(path, JSON.stringify(file, null, 2), "utf-8");
-}
-
 module.exports = {
-  splitPackages,
-  addFile,
-  readFile,
-  writeJson
+  splitPackages: function(packages, projectName, dev) {
+    let packagesString = "";
+    packages.forEach(package => {
+      packagesString += `${package} `;
+    });
+    if (dev) {
+      return `cd ${projectName} && npm install ${packagesString} --save-dev`;
+    } else {
+      return `cd ${projectName} && npm install ${packagesString}`;
+    }
+  },
+  addFile: function(path, file) {
+    fs.appendFileSync(path, file, "utf-8");
+  },
+  readFile: function(path) {
+    return fs.readFileSync(path, "utf-8");
+  },
+  writeJson: function(path, file) {
+    fs.writeFileSync(path, JSON.stringify(file, null, 2), "utf-8");
+  }
 };
