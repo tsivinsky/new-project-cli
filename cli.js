62fabcfe1ef5523d48201c7c41e70bbbf7b86f82
Daniil Tsivinsky <48863576+tsivinsky@users.noreply.github.com>
2020-02-13 14:17:36 +0300

    Add help and version messages and change comments

--- cli.js
+++ cli.js
@@ -4,27 +4,45 @@
 const Project = require("./Project");
 
 /* Arguments
-  1. Project Type
-  2. Name of new project
+  1. Project type
+  2. Name of project
   3. Additional options
 */
 
-// Get React Webpack config
+// Get config files
 const ReactWebpackConfig = require("./config/react-webpack");
 const ExpressConfig = require("./config/express-app");
 
 // Get arguments from user
 const [, , ...args] = process.argv;
 
+// Get project`s type and name from arguments
 const projectType = args[0];
 const projectName = args[1];
 
 // Get current user directory
 const userDir = process.cwd();
 
+// Return a version message
+if (args.includes("--version") || args.includes("-v")) {
+  const version = require("./package.json").version;
+  return console.log(`Version: ${version}`);
+}
+
+// Return a help message
+if (args.includes("--help") || args.includes("-h")) {
+  const help = require("./config/help");
+  return console.log(`
+Package name: ${help.name}
+Current version: ${help.version}
+${help.message}
+  `);
+}
+
 switch (projectType) {
   case "react-webpack":
     if (projectName) {
+      // Create a new React Webpack project
       const ReactWebpack = new Project({
         userDir,
         name: projectName,
@@ -37,11 +55,12 @@ switch (projectType) {
 
       ReactWebpack.create();
     } else {
-      console.log("Type a valid name of project");
+      console.log("Enter a valid name of project");
     }
     break;
   case "express-app":
     if (projectName) {
+      // Create a new Express project
       const ExpressApp = new Project({
         userDir,
         name: projectName,
@@ -54,11 +73,11 @@ switch (projectType) {
 
       ExpressApp.create();
     } else {
-      console.log("Type a valid name of project");
+      console.log("Enter a valid name of project");
     }
     break;
 
   default:
-    console.log("Type a type of project after new-project command");
+    console.log("Enter a project type after new-project command");
     break;
 }
