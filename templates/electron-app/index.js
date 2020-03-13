e6e59662867a2f1480f103695a89bdb8d4db49a4
Daniil Tsivinsky <48863576+tsivinsky@users.noreply.github.com>
2020-03-13 22:04:33 +0300

    Change electron app template

--- templates/electron-app/index.js
+++ templates/electron-app/index.js
@@ -1,21 +1,66 @@
-const electron = require("electron");
-const url = require("url");
+// Require dependencies
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+// Global variables
+let win;
+let menu;
 
-let AppWindow;
+// Menu template
+const menuTemplate = [
+  {
+    label: "App",
+    submenu: [
+      {
+        label: "Minimize",
+        accelerator: process.platform === "darwin" ? "Command+M" : "Ctrl+M",
+        click(item, focusedWindow) {
+          focusedWindow.minimize();
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// Add developer tools in development mode
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "Dev Tools",
+    submenu: [
+      {
+        label: "Toggle Dev Tools",
+        accelerator: process.platform === "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
+
+// Add empty object into menu template on Mac OS
+if (process.platform === "darwin") {
+  menuTemplate.unshift({});
+}
 
 // Create app window
 app.on("ready", function() {
-  AppWindow = new BrowserWindow({});
+  win = new BrowserWindow({});
 
   // Load html file
-  AppWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file:",
-      slashes: true
-    })
-  );
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // Append menu to application
+  menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 });
